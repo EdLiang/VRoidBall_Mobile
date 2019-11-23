60efdaa24fe9f247421ab6dd0c7748c4ecd3c953
Edward Liang <kentoruya@gmail.com>
2019-11-23 05:54:03 -0800

    Main Menu Scene
    Main Menu
    Play Button
    Scene change when play button clicked
    Initial fade in when game started

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+        FadeIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(DoFade());
+    }
+
+    IEnumerator DoFade()
+    {
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.interactable = false;
+            canvasGroup.alpha += Time.deltaTime / 3;
+            yield return null;
+        }
+        canvasGroup.interactable = true;
+        yield return null;
+    }
+}
