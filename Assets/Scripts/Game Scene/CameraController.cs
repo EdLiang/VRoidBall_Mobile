8b3d248280feba130d7812664b67a47907fe0dfe
Edward Liang <kentoruya@gmail.com>
2019-11-28 22:11:30 -0800

    Merge request commit

--- Assets/Scripts/Game Scene/CameraController.cs
+++ Assets/Scripts/Game Scene/CameraController.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    public Transform target;
     private Vector3 offset = new Vector3(0, 2, -1.5f);
+    public Transform pivot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pivot.transform.position = target.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
